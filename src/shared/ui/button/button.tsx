7d635599f0b3545aa15013d382ee77dc4c445180
Denis <buddy.den@mail.ru>
2026-07-31 23:42:32 +0400

    added twMerge for button

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,14 +1,16 @@
 import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 
 function Button({ className, children, ...props }: ComponentProps<"button">) {
-
-    return <button
-        className={ `border-1 border-primary-300 
+    const baseStyles = `border-1 border-primary-300 
             shadow-lg shadow-primary-100
             rounded-xl px-6 py-2 text-primary-foreground bg-primary-400 
             hover:cursor-pointer hover:ring-1 hover:ring-primary-200
-            transition-all` }
+            transition-all`;
+
+    return <button
+        className={ twMerge(baseStyles, className) }
         { ...props }
     >
         { children }
