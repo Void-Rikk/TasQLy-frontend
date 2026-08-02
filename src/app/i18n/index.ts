23c5ff2995c669880fbb40103434a876b350cfcc
Denis <buddy.den@mail.ru>
2026-08-02 23:42:13 +0400

    i18n fix

--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -2,10 +2,17 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import ruHome from "./locales/ru/home.json";
+import enHome from "./locales/en/home.json";
+
 i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        resources: {
+            ru: { home: ruHome },
+            en: { home: enHome }
+        },
         fallbackLng: "en",
         debug: false,
         interpolation: {
