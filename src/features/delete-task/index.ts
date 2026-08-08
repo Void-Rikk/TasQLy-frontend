fffb7f2b489241c9001075ee86b2104d9b58869f
Denis <buddy.den@mail.ru>
2026-08-08 23:38:36 +0400

    added delete task feature

---
+++ src/features/delete-task/index.ts
@@ -0,0 +1,5 @@
+import { DeleteTaskButton } from "./ui/delete-task-button.tsx";
+
+export {
+    DeleteTaskButton
+}
---
+++ src/features/delete-task/ui/delete-task-button.tsx
@@ -0,0 +1,42 @@
+import { Button } from "../../../shared/ui/button";
+import { X } from "lucide-react";
+import { useDeleteTask } from "../../../entities/task";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+
+
+interface DeleteTaskButtonProps {
+    id: string;
+}
+
+export function DeleteTaskButton({ id }: DeleteTaskButtonProps ) {
+    const [deleteTask, { loading: deletingTask }] = useDeleteTask();
+
+    const { t } = useTranslation("home");
+
+    const onDelete = () => {
+        const promiseForToast = deleteTask({
+            variables: {
+                id
+            }
+        });
+
+        toast.promise(promiseForToast, {
+            loading: t("deleteTaskStatus.loading"),
+            error: t("deleteTaskStatus.error"),
+            success: t("deleteTaskStatus.success")
+        })
+    }
+
+    return (
+        <Button
+            className={ `opacity-0 p-1 size-6 flex justify-center items-center group-hover:opacity-100` }
+            onClick={ onDelete }
+            disabled={ deletingTask }
+        >
+            <X
+                className={ `text-(--danger) size-full` }
+            />
+        </Button>
+    );
+}
