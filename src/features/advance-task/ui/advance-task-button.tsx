fd1f88168906a25e75d4b05f43df339205443aab
Denis <buddy.den@mail.ru>
2026-08-09 10:43:02 +0400

    added advance task feature

--- src/features/advance-task/ui/advance-task-button.tsx
+++ src/features/advance-task/ui/advance-task-button.tsx
@@ -1,5 +1,46 @@
+import { Button } from "../../../shared/ui/button";
+import { type TaskStatus, useAdvanceTask } from "../../../entities/task";
+import { useTranslation } from "react-i18next";
+import { MoveRight } from "lucide-react";
+import toast from "react-hot-toast";
 
 
-export function AdvanceTaskButton() {
+interface AdvanceTaskButtonProps {
+    id: string;
+    status: TaskStatus;
+}
 
+export function AdvanceTaskButton({ id, status }: AdvanceTaskButtonProps) {
+    const [advanceTask, { loading: advancingTask }] = useAdvanceTask();
+
+    const { t } = useTranslation("home");
+
+    const onAdvance = () => {
+        const promiseForToast = advanceTask({
+            variables: {
+                id
+            }
+        });
+
+        toast.promise(promiseForToast, {
+            success: t("advanceTaskStatus.success"),
+            error: t("advanceTaskStatus.error"),
+            loading: t("advanceTaskStatus.loading"),
+        })
+    }
+
+    return (
+        <Button
+            className={ `flex gap-1 items-center
+            py-0 px-2
+            text-xs text-(--text-muted) hover:text-(--text)` }
+            onClick={ onAdvance }
+            disabled={ advancingTask || status === "DONE" }
+        >
+            { t("taskAdvanceButton") }
+            <MoveRight
+                className={ `w-3` }
+            />
+        </Button>
+    );
 }
