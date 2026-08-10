c2b93e2a652a30d8220ac7bda17181aeb222b42a
Denis <buddy.den@mail.ru>
2026-08-10 18:07:40 +0400

    fixed overlay ui component

--- src/shared/ui/overlay/overlay.tsx
+++ src/shared/ui/overlay/overlay.tsx
@@ -9,7 +9,7 @@ export function Overlay({ className }: OverlayProps) {
 
     return (
         <div
-            className={ twMerge("absolute inset-0 bg-[rgba(0,0,0,0.25)] pointer-events-none", className || "") }
+            className={ twMerge("absolute inset-0 rounded-xl bg-[rgba(0,0,0,0.25)] pointer-events-none", className || "") }
         >
         </div>
     );
